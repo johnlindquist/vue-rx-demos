41b9f473c2d0e36c7de45e75b378fc49e3283535
johnlindquist <johnlindquist@gmail.com>
2018-03-26 18:05:45 -0500

    tabs-start

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="section">
     <b-tabs v-model="activeTab">
-      <b-tab-item label="I'm a tab">
+      <b-tab-item label="Tab label goes here">
       </b-tab-item>
     </b-tabs>
-    <h1 class="title">The "UI/Content Sync" Scenario</h1>
+    <h1 class="title">Sync UI and Content</h1>
     <img src="" alt="">
   </section>
 </template>
