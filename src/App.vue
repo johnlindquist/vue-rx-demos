3a641b76cb5342b2804911cb95b9a4eaa206f7d5
johnlindquist <johnlindquist@gmail.com>
2018-03-26 14:39:39 -0500

    snooze

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,48 @@
 <template>
 
   <section class="section">
-    <div v-if="!done$">
-      <h1 class="title">{{output$}}</h1>
-      <b-modal :active="stillThere$">
-
-        <div class="content">
-          <h1 class="title has-text-white">Still there?</h1>
-          <h1 class="title has-text-white">{{countdown$}}</h1>
-        </div>
-
-      </b-modal>
-    </div>
-    <div v-else>
-      <h1 class="title">Bye!</h1>
-    </div>
+    <h1 class="title">{{text$}}</h1>
+    <button :disabled="active$" v-stream:click="snooze$" class="button is-success">Snooze</button>
+    <button :disabled="active$" v-stream:click="dismiss$" class="button is-danger">Dismiss</button>
   </section>
 </template>
 
 <script>
+console.clear()
 import { Observable } from "rxjs"
 
 export default {
+  domStreams: ["dismiss$", "snooze$"],
+
   subscriptions() {
-    const keys$ = Observable.fromEvent(
-      window,
-      "keydown"
-    )
-      .pluck("key")
-      .scan((text, key) => text + key)
+    const text$ = Observable.interval(250)
+      .startWith(5)
+      .scan(time => time - 1)
+      .takeWhile(time => time > 0)
+      .concat(Observable.of("Wake up!"))
+      .do(console.log.bind(console))
 
-    const mousemove$ = Observable.fromEvent(
-      window,
-      "mousemove"
-    ).map(({ clientX, clientY }) => ({
-      clientX,
-      clientY
-    }))
+      .repeatWhen(() => {
+        console.log("repeat when")
+        return this.snooze$.takeUntil(
+          this.dismiss$
+        )
+      })
+      .concat(
+        Observable.of("Have a great day! 🤗")
+      )
+      .share()
 
-    const eventsTimeout$ = Observable.merge(
-      keys$,
-      mousemove$
-    ).switchMapTo(
-      Observable.timer(1000).mapTo("Still there?")
-    )
-
-    const stillThere$ = Observable.merge(
-      mousemove$.mapTo(false),
-      keys$.mapTo(false),
-      eventsTimeout$.mapTo(true)
-    )
-
-    const countdown$ = stillThere$.switchMap(() =>
-      Observable.interval(1000)
-        .startWith(5)
-        .scan(remaining => remaining - 1)
-        .takeWhile(i => i >= 0)
-    )
-
-    const done$ = countdown$
-      .filter(value => value === 0)
-      .mapTo(true)
-      .startWith(false)
-
-    const output$ = Observable.merge(
-      mousemove$,
-      keys$
-    )
+    const active$ = text$
+      .map(
+        value =>
+          value === "Wake up!" ? false : true
+      )
+      .startWith(true)
 
     return {
-      output$,
-      stillThere$,
-      countdown$,
-      done$
+      text$,
+      active$
     }
   }
 }
