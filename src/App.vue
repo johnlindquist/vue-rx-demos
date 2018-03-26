ab0603737cd0739ae50c68c4a6e8b1c5ccca0028
johnlindquist <johnlindquist@gmail.com>
2018-03-26 00:47:54 -0500

    button-mashing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
 
+  <section class="section">
+
+    <h1 class="title">{{text$}}</h1>
+    <form @submit.prevent="submitHandler">
+      <input type="text" class="input" @input="inputHandler">
+      <input type="submit" class="button" :disabled="disable$" :value="buttonText$">
+    </form>
+  </section>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Observable } from "rxjs"
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  observableMethods: [
+    "submitHandler",
+    "inputHandler"
+  ],
+  subscriptions() {
+    const input$ = this.inputHandler$
+      .pluck("target", "value")
+      .share()
+
+    const [empty$, term$] = input$
+      .startWith("")
+      .partition(term => term === "")
+
+    const formatResponse = res =>
+      res.response.length
+        ? res.response
+            .map(person => person.name)
+            .join(", ")
+        : "No results 😭"
+
+    const search = term =>
+      Observable.ajax(
+        `http://localhost:3000/people?delay=1000&name_like=${term}`
+      )
+        .map(formatResponse)
+        .share()
+
+    const result$ = this.submitHandler$
+      .withLatestFrom(term$, (_, term) => term)
+      .exhaustMap(term =>
+        Observable.race(
+          input$.mapTo("Cancelled 😢").take(1),
+          search(term).takeUntil(input$)
+        )
+      )
+
+    const text$ = Observable.merge(
+      this.submitHandler$.mapTo("Loading..."),
+      empty$.mapTo("Please type something!"),
+      term$.mapTo("Hit enter when ready"),
+      result$
+    )
+
+    const disable$ = Observable.merge(
+      this.submitHandler$.mapTo(true),
+      empty$.mapTo(true),
+      term$.mapTo(false)
+    ).startWith(true)
+
+    const buttonText$ = Observable.merge(
+      this.submitHandler$.mapTo("Loading..."),
+      empty$.mapTo("Empty"),
+      term$.mapTo("Search"),
+      result$.mapTo("Change to Search Again")
+    )
+
+    return {
+      text$,
+      disable$,
+      buttonText$
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import "buefy/lib/buefy.css"
+
+import Vue from "vue"
+import Buefy from "buefy"
+import Rx from "rxjs/Rx"
+import VueRx from "vue-rx"
+import VueAxios from "vue-axios"
+import axios from "axios"
+import App from "./App.vue"
+
+Vue.use(Buefy)
+Vue.use(VueRx, Rx)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
