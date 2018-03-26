8b981842d5192648753c9f4391fc1d0b8250c7d7
johnlindquist <johnlindquist@gmail.com>
2018-03-26 18:04:47 -0500

    still-there-start

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-
   <section class="section">
-    <b-notification :active="done$">
-      <h1>Signed out</h1>
-    </b-notification>
-    <h1 class="title">{{output$}}</h1>
-    <div class="subtitle">{{events$}}</div>
+    <h1 class="title">Are You Still There?</h1>
   </section>
 </template>
 
@@ -14,50 +9,7 @@ import { Observable } from "rxjs"
 
 export default {
   subscriptions() {
-    const keys$ = Observable.fromEvent(
-      window,
-      "keydown"
-    )
-      .pluck("key")
-      .scan((text, key) => text + key)
-
-    const mouse$ = Observable.fromEvent(
-      window,
-      "mousemove"
-    ).map(event => ({
-      x: event.clientX,
-      y: event.clientY
-    }))
-
-    const events$ = Observable.merge(
-      keys$,
-      mouse$
-    )
-
-    const countdown$ = Observable.interval(1000)
-      .startWith(3)
-      .scan(time => time - 1)
-      .takeWhile(time => time >= 0)
-
-    const output$ = events$
-      .switchMapTo(
-        Observable.of("Keep it up! 👍").concat(
-          countdown$.delay(1000)
-        )
-      )
-      .takeWhile(value => value != 0)
-      .concat(Observable.of("We'll miss you! 🤧"))
-
-    return {
-      events$,
-      output$,
-      done$: Observable.of(false).concat(
-        output$
-          .last()
-          .delay(1000)
-          .mapTo(true)
-      )
-    }
+    return {}
   }
 }
 </script>
