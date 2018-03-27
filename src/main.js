519ff8fbca9f97ca11c5c67cfc7f786562ab9219
johnlindquist <johnlindquist@gmail.com>
2018-03-26 23:20:48 -0500

    tabs-start

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import Vue from "vue"
 import Buefy from "buefy"
 import Rx from "rxjs/Rx"
 import VueRx from "vue-rx"
-import VueAxios from "vue-axios"
-import axios from "axios"
+
 import App from "./App.vue"
 
 Vue.use(Buefy)
 Vue.use(VueRx, Rx)
-Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
