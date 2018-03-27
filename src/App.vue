2946861d56dbe9e21285b501b6cd10e430452e47
johnlindquist <johnlindquist@gmail.com>
2018-03-27 01:03:53 -0500

    retry

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,21 @@
 <template>
-
   <section class="section">
-
-    <h1 class="title">{{text$}}</h1>
-    <form @submit.prevent="submitHandler">
-      <input type="text" class="input" @input="inputHandler">
-      <input type="submit" class="button" :disabled="disable$" :value="buttonText$">
-    </form>
+    <button v-stream:click="load$" class="button">Load</button>
+    <h1 class="title">{{name$}}</h1>
+    <img :src="image$" alt="">
   </section>
 </template>
 <script>
 import { Observable } from "rxjs"
-
 export default {
-  observableMethods: [
-    "submitHandler",
-    "inputHandler"
-  ],
+  domStreams: ["load$"],
   subscriptions() {
-    const input$ = this.inputHandler$
-      .pluck("target", "value")
-      .share()
-
-    const [empty$, term$] = input$
-      .startWith("")
-      .partition(term => term === "")
-
-    const formatResponse = res =>
-      res.response.length
-        ? res.response
-            .map(person => person.name)
-            .join(", ")
-        : "No results 😭"
-
-    const search = term =>
-      Observable.ajax(
-        `http://localhost:3000/people?delay=1000&name_like=${term}`
-      )
-        .map(formatResponse)
-        .share()
-
-    const result$ = this.submitHandler$
-      .withLatestFrom(term$, (_, term) => term)
-      .exhaustMap(term =>
-        Observable.race(
-          input$.take(1).mapTo("Cancelled 😢"),
-          search(term).takeUntil(input$)
-        )
-      )
-
-    const text$ = Observable.merge(
-      this.submitHandler$.mapTo("Loading..."),
-      empty$.mapTo("Please type something!"),
-      term$.mapTo("Hit enter when ready"),
-      result$
-    )
-
-    const disable$ = Observable.merge(
-      this.submitHandler$.mapTo(true),
-      empty$.mapTo(true),
-      term$.mapTo(false)
-    ).startWith(true)
-
-    const buttonText$ = Observable.merge(
-      this.submitHandler$.mapTo("Loading..."),
-      empty$.mapTo("Empty"),
-      term$.mapTo("Search"),
-      result$.mapTo("Change to Search Again")
-    )
+    const name$ = Observable.of("Some Name")
+    const image$ = Observable.of("Image")
 
     return {
-      text$,
-      disable$,
-      buttonText$
+      name$,
+      image$
     }
   }
 }
