2818c930773442f0b95852727d90a451904e59e1
johnlindquist <johnlindquist@gmail.com>
2018-03-26 21:40:21 -0500

    slides

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <section class="section">
-    <h1 class="title">Button Mashing</h1>
-    <form>
-      <input type="text" class="input">
-      <input type="submit" class="button">
-    </form>
+    <h1 class="title">
+      {{slide$}}
+    </h1>
   </section>
 </template>
 <script>
@@ -12,7 +10,43 @@ import { Observable } from "rxjs"
 
 export default {
   subscriptions() {
-    return {}
+    const keys$ = Observable.fromEvent(
+      window,
+      "keydown"
+    ).pluck("key")
+
+    const left$ = keys$.filter(
+      key => key === "ArrowLeft"
+    )
+    const right$ = keys$.filter(
+      key => key === "ArrowRight"
+    )
+
+    const index$ = Observable.merge(
+      left$.mapTo(-1),
+      right$.mapTo(1)
+    )
+      .throttleTime(250)
+      .startWith(0)
+      .scan((i, direction) => i + direction)
+
+    const slide$ = Observable.of([
+      "Hi, I'm John Lindquist",
+      "egghead.io founder",
+      "vue-rx",
+      "RxJS isn't easy",
+      "I'm not smart",
+      '"Badass: Making Users Awesome" - Kathy Sierra',
+      "Perceptual Exposure",
+      "Your Brain Will Detect Patterns"
+    ]).combineLatest(
+      index$,
+      (slides, index) => slides[index]
+    )
+
+    return {
+      slide$
+    }
   }
 }
 </script>
